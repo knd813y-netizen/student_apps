42702f6b71ea1c157d09a9add08af9835be7d887
knd813y-netizen <knd813.y@gmail.com>
2026-05-20 11:20:17 +0900

    TestListSubject.javaの変更

--- src/main/java/bean/TestListSubject.java
+++ src/main/java/bean/TestListSubject.java
@@ -12,7 +12,7 @@ public class TestListSubject implements Serializable {
 	private String classNum;
 	// 2026/05/19 変更理由: getPoint(key: int): String に合わせ、成績参照画面で扱う点数を文字列で保持する。
 	// TestListSubjectDao からは int で渡されるため、putPoint で String に変換する。
-	private Map<Integer, String> points = new LinkedHashMap<>();
+	private Map<Integer, Integer> points = new LinkedHashMap<>();
 	
 // ゲッター(Getter)とセッター(Setter)の設置
 	public int getEntYear() {
@@ -27,7 +27,7 @@ public class TestListSubject implements Serializable {
 	public String getClassNum() {
 		return classNum;
 	}
-	public Map<Integer, String> getPoints() {
+	public Map<Integer, Integer> getPoints() {
 		return points;
 	}
 
@@ -44,7 +44,7 @@ public class TestListSubject implements Serializable {
 	public void setClassNum(String classNum) {
 		this.classNum = classNum;
 	}
-	public void setPoints(Map<Integer, String> points) {
+	public void setPoints(Map<Integer, Integer> points) {
 		this.points = points;
 	}
 
@@ -53,14 +53,14 @@ public class TestListSubject implements Serializable {
 	 * 2026/05/19 変更理由: DBから取得した数値の点数を、表示用のStringとして保持するため。
 	 */
 	public void putPoint(int key, int value) {
-		this.points.put(key, String.valueOf(value));
+		this.points.put(key, value);
 	}
 
 	/**
 	 * 指定回数の点数を取得
 	 * 2026/05/19 変更理由: getPoint(key: int): String と一致させるため。
 	 */
-	public String getPoint(int key) {
+	public Integer getPoint(int key) {
 		return this.points.get(key);
 	}
 }
